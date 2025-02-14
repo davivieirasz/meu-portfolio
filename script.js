653401a39210adc54b2ef3b3f8ec52df99e7ba95
Davi Vieira <134980379+Davivieirasz@users.noreply.github.com>
2025-02-14 11:50:22 -0300

    Meu Primeiro Projeto! 🚀
    Esse é meu primeiro projeto, e estou em constante aprendizado, buscando evoluir minhas habilidades.
    Criei meu portfólio utilizando HTML, CSS e um pouco de Javascript.

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const elements = document.querySelectorAll('.fade-in');
+
+    const options = {
+        root: null, 
+        rootMargin: '0px',
+        threshold: 0.1 
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            } else {
+                entry.target.classList.remove('visible'); 
+            }
+        });
+    }, options);
+
+    elements.forEach(element => {
+        observer.observe(element);
+    });
+});
